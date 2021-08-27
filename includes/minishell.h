a741a941ba8e62f77e6a58888e4a6d364e56c2f2
melaena <melaena@student.21-school.ru>
2021-08-27 19:03:54 +0300

    initial commit

---
+++ includes/minishell.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: melaena <melaena@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/27 18:56:16 by melaena           #+#    #+#             */
+/*   Updated: 2021/08/27 19:01:11 by melaena          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <signal.h>
+# include <errno.h>
+# include <sys/wait.h>
+# include <limits.h>
+# include <curses.h>
+# include <term.h>
+
+# include <libft.h>
+
+
+#endif

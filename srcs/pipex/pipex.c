b092cf599e640f76559a8958b633acd4375ac535
Microdosing Elaena <melaena@mi-c5.msk.21-school.ru>
2021-09-08 20:15:47 +0300

    need to apply funcs managament and test all cases. Need to add signal handler in heredoc. Signal handler to multishell use. As well inc SHLVL. All is working almost right

--- srcs/pipex/pipex.c
+++ srcs/pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: melaena <melaena@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 18:02:01 by kbulwer           #+#    #+#             */
-/*   Updated: 2021/09/08 19:44:23 by melaena          ###   ########.fr       */
+/*   Updated: 2021/09/08 20:11:09 by melaena          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ void	child_process(t_sect *elem)
 	close_pipeline(elem);
 	args = sect_form_args(elem);
 	code = exec_command(args);
+	exit(code);
 }
 
 void	bi_child_process(t_sect *elem)
@@ -40,6 +41,21 @@ void	bi_child_process(t_sect *elem)
 	set_exit_status(code);
 }
 
+int		get_cmd_count(t_sect *elem)
+{
+	int	size;
+
+	size = 0;
+	while (elem)
+	{
+		if (elem->type == SECT_TYPE_CMD)
+			size += 1;
+		elem = elem->next;
+	}
+	return (size);
+}
+
+
 void	pipex(t_sect *elem)
 {
 	int		status;
@@ -47,32 +63,33 @@ void	pipex(t_sect *elem)
 
 	status = 0;
 	temp = elem;
-	while (elem)
+	if (get_cmd_count(elem) == 1 && is_builtin(elem->content))
 	{
-		if (elem->type == SECT_TYPE_CMD)
+		bi_child_process(elem);
+		close_pipeline(elem);
+	}
+	else
+	{
+		while (elem)
 		{
-			if (is_builtin(elem->content))
-			{
-				bi_child_process(elem);
-			}
-			else
+			if (elem->type == SECT_TYPE_CMD)
 			{
 				elem->pid = fork();
 				if (elem->pid == 0)
 					child_process(elem);
 			}
+			elem = elem->next;
 		}
-		elem = elem->next;
-	}
-	elem = temp;
-	close_pipeline(elem);
-	while (elem)
-	{
-		if (elem->type == SECT_TYPE_CMD && (!is_builtin(elem->content)))
+		elem = temp;
+		close_pipeline(elem);
+		while (elem)
 		{
-			waitpid(elem->pid, &status, 0);
-			set_exit_status(WEXITSTATUS(status));
+			if (elem->type == SECT_TYPE_CMD && (!is_builtin(elem->content)))
+			{
+				waitpid(elem->pid, &status, 0);
+				set_exit_status(WEXITSTATUS(status));
+			}
+			elem = elem->next;
 		}
-		elem = elem->next;
 	}
 }
